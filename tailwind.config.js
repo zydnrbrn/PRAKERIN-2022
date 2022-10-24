ebbfeb7eaa8416d10c827a7231d3923d039501c2
zydnrbrn <zidanreborn@gmail.com>
2022-10-24 15:02:47 +0700

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+ module.exports = {
+    darkMode: 'class',
+    content: ["./src/**/*.{html,js}","index.html"],
+  theme: {
+    fontFamily: {
+        'righteous': ['Righteous']
+    },
+    extend: {
+      colors: {
+        'main-red': '#BB3853'
+      }
+    }
+  }
+}
